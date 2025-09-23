66c212a27b9049d2d6191e9cd27f8b4e3447a006
Nahuel <matiasghilardisalinas@gmail.com>
2025-09-23 15:33:15 -0300

    Crear la plantilla basica para probar la prediccion del modelo, luego falta crear la logica para tratar las imagenes y llamar la prediccion del modeo

--- src/api/main.py
+++ src/api/main.py
@@ -31,3 +31,44 @@ except Exception as e:
 @app.get("/")
 def read_root():
     return {"message": "Bienvenido a la API de Nimbus AI"}
+
+
+# --- 5. Definir el "Contrato" de Entrada de Datos ---
+# Esta clase le dice a FastAPI qué datos esperamos recibir para hacer una predicción.
+class FeaturesInput(BaseModel):
+    PRCP: float
+    SNWD: float
+    TAVG: float
+    TMAX: float
+    TMIN: float
+    om_weather_code: float
+    om_rain_sum: float
+    om_snowfall_sum: float
+    om_precipitation_hours: float
+    om_wind_gusts_10m_max: float
+    om_wind_direction_10m_dominant: float
+    om_shortwave_radiation_sum: float
+    om_et0_fao_evapotranspiration: float
+    om_dew_point_2m_mean: float
+    om_relative_humidity_2m_mean: float
+    om_pressure_msl_mean: float
+    mes: int
+    dia_del_año: int
+    rango_temp_diario: float
+    latitude: float
+    longitude: float
+
+# --- 6. Crear el Endpoint de Predicción ---
+# @app.post le dice a FastAPI que este endpoint recibirá datos.
+@app.post("/predict")
+def predict_hail(input_features: FeaturesInput):
+    # Por ahora, solo confirmaremos que recibimos los datos y devolveremos una respuesta de prueba.
+    
+    print("Datos recibidos en el endpoint /predict:")
+    print(input_features.dict())
+    
+    # En el próximo paso, aquí irá la lógica para procesar los datos,
+    # obtener la imagen satelital y llamar al modelo.
+    
+    # Devolvemos una respuesta de ejemplo
+    return {"probabilidad_granizo_estimada": 0.5, "status": "Prueba exitosa"}
