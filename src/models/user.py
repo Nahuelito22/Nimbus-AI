4d5ef42ed4fb3e1f7643c4dc3da3095d32fc5128
Nahuel <matiasghilardisalinas@gmail.com>
2025-11-05 21:39:52 -0300

    Se modificaron los campos de contraseña que causaban conflictos

--- src/models/user.py
+++ src/models/user.py
@@ -8,7 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     role = db.Column(db.String(80), nullable=False, default='user')
     is_verified = db.Column(db.Boolean, default=False)
     is_suspended = db.Column(db.Boolean, default=False, nullable=False)
